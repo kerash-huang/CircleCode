afde8eccab116c2a8cd2bb88f5c06d7ac5bc01e7
Kerash <blog@kerash.tw>
2015-11-18 11:23:01 +0800

    Update CircleCode

--- CircleCode.py
+++ CircleCode.py
@@ -1,14 +1,15 @@
 # coding: utf8
-import sublime, sublime_plugin
+
+import sublime_plugin
+
 
 class CircleCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        selection    = self.view.sel()
-        selectionLen = len(selection)
-        content      = self.view.substr(sublime.Region(0, self.view.size())).encode('utf8')
-
+        selection = self.view.sel()
+        #selectionLen = len(selection)
+        #content = self.view.substr(sublime.Region(0, self.view.size())).encode('utf8')
         # 多個 select item
-        for sel  in selection:
+        for sel in selection:
             if not sel.empty():
                 seltext = self.view.substr(sel)
                 seltext = self.circle(seltext)
